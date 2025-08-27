2f7cacffc605c3c49bf80d6dade3c301c9bcdbec
Jayland <veaseyjayland3@gmail.com>
2025-08-26 20:43:16 -0500

    Navbar started

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <main className="p-8">
-      <h1 className="text-3xl font-bold">Hello World</h1>
-      <p>This is the beginning.</p>
-    </main>
-  );
+  return <main></main>;
 }
