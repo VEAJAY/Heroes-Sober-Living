0a7166db5450e1d9236cb8ecf304eb3e57c8e516
Jayland <veaseyjayland3@gmail.com>
2025-08-26 19:55:21 -0500

    Starting application code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import { hostname } from "os";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+    root: "C:\\Users\\vease\\OneDrive\\Documents\\heroes-sober-living",
+  },
+  images: {
+    remotePatterns: [{ hostname: "" }],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
