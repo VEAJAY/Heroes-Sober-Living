7a2cefea3dc9290f5efb21af811f1574eb076d40
Jayland <veaseyjayland3@gmail.com>
2025-08-26 20:44:43 -0500

    Revert "Navbar started"
    This reverts commit 2f7cacffc605c3c49bf80d6dade3c301c9bcdbec.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">Hello World</h1>
+      <p>This is the beginning.</p>
+    </main>
+  );
 }
