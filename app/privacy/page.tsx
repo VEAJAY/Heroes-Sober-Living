d4af4d6b624d69368a0dbf6e4d77e8e65a57b492
Jayland <veaseyjayland3@gmail.com>
2025-08-30 20:38:42 -0500

    Added privacy policy

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,9 +1,195 @@
-import Message from "../components/message";
-
 export default function Privacy() {
   return (
-    <div>
-      <Message title="Attention!" />
+    <div className="flex flex-col justify-center items-center p-4 sm:p-8">
+      <div className="max-w-7xl">
+        <h1 className="text-2xl mt-20 sm:text-5xl font-serif font-bold text-left">
+          Privacy Policy for <i>Heroes & Sober Living</i>
+        </h1>
+        <p className="mt-8 text-xl text-left">
+          This privacy policy (the “Privacy Policy” or “Policy”) of Sagora
+          Senior Living Inc. (“Company,” “we,” “us,” or “our”) describes our
+          privacy practices regarding personal information we collect, including
+          from persons (“you” or “your”) who access our property’s website or
+          our related mobile applications that may be available for download
+          (collectively, the “Site”).
+        </p>
+        <p className="font-bold text-xl mt-8">Effect Date: August 30th, 2025</p>
+
+        {/* INTRODUCTION */}
+        <h1 className="text-3xl font-serif font-bold text-left mt-8">
+          Introduction
+        </h1>
+        <p className="mt-8 text-xl text-left">
+          Heroes & Sober Living (“we,” “our,” or “us”) respects the privacy of
+          our residents, visitors, and website users. This Privacy Policy
+          explains how we collect, use, disclose, and protect personal
+          information for veterans and other individuals residing at or
+          interacting with our group home. We are committed to complying with
+          all applicable federal and state privacy laws, including the Health
+          Insurance Portability and Accountability Act (HIPAA), where
+          applicable, and the Privacy Act for veteran-related information.
+        </p>
+        <p className="mt-8 text-xl text-left">
+          THE SITE IS NOT INTENDED FOR CHILDREN AND COMPANY DOES NOT KNOWINGLY
+          SOLICIT OR COLLECT PERSONAL INFORMATION ON THE SITE FROM CHILDREN
+          UNDER THE AGE OF 13. IF COMPANY LEARNS THAT A CHILD UNDER THE AGE OF
+          13 HAS SUBMITTED PERSONALLY IDENTIFIABLE INFORMATION TO COMPANY
+          THROUGH THE SITE, IT WILL TAKE REASONABLE MEASURES TO DELETE SUCH
+          INFORMATION FROM ITS RECORDS AND TO NOT USE SUCH INFORMATION FOR ANY
+          PURPOSE (EXCEPT WHERE NECESSARY TO PROTECT THE SAFETY OF THE CHILD OR
+          OTHERS, AS REQUIRED BY LAW, OR AS MAY OTHERWISE BE REQUIRED BY LAW).
+        </p>
+        <p className="mt-8 text-xl text-left">
+          <b>YOUR CONSENT AND POLICY CHANGES.</b> PLEASE READ THIS ENTIRE
+          PRIVACY POLICY CAREFULLY. BY USING THE SITE, YOU EXPRESSLY CONSENT,
+          WITHOUT QUALIFICATION, TO THE COLLECTION, USE AND DISCLOSURE OF
+          INFORMATION, INCLUDING PERSONAL INFORMATION, AS DESCRIBED IN THIS
+          PRIVACY POLICY. IF YOU DO NOT AGREE TO THE TERMS OF THIS PRIVACY
+          POLICY, YOU ARE NOT AUTHORIZED TO USE THE SITE.
+        </p>
+        <p className="mt-8 text-xl text-left">
+          Company may amend this Privacy Policy from time to time. We will post
+          those changes on the website or update the Privacy Policy date below.
+          In certain cases, you will be notified via email or by a notice on our
+          website.
+        </p>
+        {/* INFORMATION */}
+        <h1 className="text-3xl font-serif font-bold text-left mt-8">
+          Information We Collect & Use
+        </h1>
+        <p className="mt-8 text-xl text-left">
+          We collect, process and store various types of Personal Information.
+          For purposes of this Policy, “Personal Information” means information
+          that identifies, relates to, describes, is reasonably capable of being
+          associated with, or could reasonably be linked, directly or
+          indirectly, with a particular consumer or household. It does not
+          include de-identified or aggregate information, or public information
+          lawfully available from governmental records.
+        </p>
+        <p className="mt-8 text-xl text-left">
+          Personal Information we collect may include: your name, email
+          addresses, postal addresses, postal codes, user Internet Protocol (IP)
+          addresses, social security/insurance number, driver’s license number,
+          prior addresses, monthly rent paid at prior residences, employers, job
+          titles, income, telephone numbers, credit card information, bank
+          account information and routing numbers, date of birth, gender, age,
+          marital status, user name and password (if you choose to become a
+          registered user of the Site or our products and services), emergency
+          contact information, information about your pets, information about
+          your vehicles including license plate numbers, biometric information
+          or biometric identifiers (if you choose to schedule a self-guided tour
+          and/or use our identity verification services), when a smart device is
+          on or off; when a door is locked, unlocked, opened, or closed; the
+          unit with which the door is associated (if you choose to use Smart
+          Home or the Home IQ Device(s) or Home IQ Hub), and other information
+          either desirable or necessary to provide quality services to our
+          residents and visitors. We may also collect information about you such
+          as your use of the Site and communication preferences.
+        </p>
+        <p className="mt-8 text-xl text-left">
+          Your Personal Information may be retained for as long as you use the
+          Site or our products and services. Retention periods may be extended
+          if we are required to preserve your personal information because of
+          litigation, investigations and other similar proceedings, or if a
+          longer retention period is required or permitted by applicable law.
+        </p>
+        <p className="mt-8 text-xl text-left">
+          We do not and will not sell your Personal Information.
+        </p>
+        <p className="mt-8 text-xl text-left">
+          <b>USE OF COOKIES AND OTHER TECHNOLOGIES.</b> Like many other website
+          operators, Company may use cookies to customize content specific to
+          your interests and track your use of the Site. “Cookies” are text
+          files that are placed on your computer and may be used to store your
+          personal information. In addition to cookies, Company may use other
+          now known or later developed technologies, to assist Company in
+          observing the behavior of users visiting the Site. These technologies
+          access individual user information but the information is used only to
+          compile aggregated statistics about the Site’s usage.
+        </p>
+        <p className="mt-8 text-xl text-left">
+          Our service providers may use cookies and those cookies may be stored
+          on your computer when you visit our website. You can choose to delete
+          existing cookies, allow or block cookies, and set preferences for
+          certain websites in your browser’s settings. Please note you may not
+          be able to use the full functionality of the Site if you choose to
+          block all cookies.
+        </p>
+        <p className="mt-8 text-xl text-left">
+          <b>EMAIL AND TEXT MANAGEMENT.</b> You may receive emails and texts
+          from Company for a variety of reasons. We respect your desire to
+          manage email and text correspondence. If you have an account with
+          Company on the Site, you can select your preferences through your
+          account settings. You can also manage your receipt of certain types of
+          communication by following the instructions included in the email we
+          send you. Please note that, even if you unsubscribe from certain email
+          correspondences, we may still need to email you with information
+          relating to your unit, the management of the property, and other
+          important transactional or administrative information.
+        </p>
+        <p className="mt-8 text-xl text-left">
+          <b>ACCESSING, REVIEWING AND CHANGING YOUR CONTACT INFORMATION.</b> As
+          an account holder, you can see, review and change most of your contact
+          information associated with your Account Profile by logging into your
+          account and editing your Account Profile. For assistance, you may
+          contact us. With some exceptions, you have the right to see what
+          Personal Information we hold about you. We can help you identify what
+          records we might have about you. We may need to confirm your identity
+          before providing you with this access, and if we cannot verify your
+          identity with the degree of certainty required, we will not be able to
+          respond to your request. If we cannot give you access, we will tell
+          you within 45 days of receipt of your request and provide you with a
+          reason, as best we can, as to why we cannot give you access.
+        </p>
+        <p className="mt-8 text-xl text-left">Residents have the right to:</p>
+        <ul className="list-disc pl-6 mt-2 text-xl text-left space-y-2">
+          <li>
+            Access, review, and request correction of their personal
+            information.
+          </li>
+          <li>
+            Request restrictions on certain uses and disclosures of their
+            information.
+          </li>
+          <li>Obtain a copy of this Privacy Policy at any time.</li>
+          <li>
+            File complaints regarding privacy violations without fear of
+            retaliation.
+          </li>
+        </ul>
+        <p className="mt-8 text-xl text-left">
+          <b>PERSONAL INFORMATION OF MINORS.</b> Our Sites are not directed to
+          minors under the age of 16 and we do not knowingly collect Personal
+          Information from minors. We may collect Personal Information of
+          minors, provided by parents or guardians, in connection with the
+          application and rental of a unit, or the provision of property
+          management services. We do not sell the Personal Information of
+          minors.
+        </p>
+        {/* UPDATES */}
+        <h1 className="text-3xl font-serif font-bold text-left mt-8">
+          Updates to This Policy
+        </h1>
+        <p className="mt-8 text-xl text-left">
+          We may update this Privacy Policy periodically. The updated version
+          will be posted on our website with a revised “Effective Date.” We
+          encourage residents and users to review this policy regularly.
+        </p>
+        {/* CONTACT INFORMATION */}
+        <h1 className="text-3xl font-serif font-bold text-left mt-8">
+          Contact Information
+        </h1>
+        <ul className="text-xl mt-8">
+          <li>
+            <b>Privacy Officer</b>
+          </li>
+          <li>Heroes & Sober Living</li>
+          <li>Greenville, Texas 75401</li>
+          <li>(469) 456-6032</li>
+          <li>(903) 441-7512</li>
+          <li>veteranssoberliving@gmail.com</li>
+        </ul>
+      </div>
     </div>
   );
 }
